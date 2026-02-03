4a9f2b1581d29839f1a1a72cf87e51642dc1d599
AnnaBlyudova <anna.blyudova.qa@gmail.com>
2026-02-03 23:16:22 +0300

    feat: complete lesson 1 tasks

---
+++ Lesson_1/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def number_print(num):
+    print(num, end='')
+
+
+number_print(8)
+number_print(8)
+number_print(0)
+number_print(0)
+number_print(5)
+number_print(5)
+number_print(5)
+number_print(3)
+number_print(5)
+number_print(3)
+number_print(5)
